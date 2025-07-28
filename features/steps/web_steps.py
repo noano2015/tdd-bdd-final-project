017cff27dad7becfdb1054558521d3cf2c7942e8
Noano2015 <pamdcv@gmail.com>
2025-07-27 20:40:13 -0400

    Done

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -152,3 +152,13 @@ def step_impl(context, message):
         )
     )
     assert(found)
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
